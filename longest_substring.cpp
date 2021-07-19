bf8e2381d4b17cd4b236ed955dc4921afb76acd0
Pratik Kamble <62785111+Git-Pratik97@users.noreply.github.com>
2021-07-19 10:22:09 +0530

    Add files via upload

---
+++ longest_substring.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s; cin >> s;
+    vector<int> dict(256, -1);
+
+    int start = -1;
+    int maxlen = 0;
+
+    for(int i = 0; i < s.size(); i++)
+    {
+        while(dict[s[i]] > start)
+        {
+            start = dict[s[i]]; 
+        }
+        dict[s[i]] = i;
+        maxlen = max(maxlen, i - start);
+    }
+
+    cout << maxlen << endl;
+
+    return 0;
+}
